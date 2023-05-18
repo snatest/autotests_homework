0c3e65c1fe144e5d08b081437296a274fe374bdd
eguzev <sna.test0012@mail.ru>
2023-05-18 18:07:48 +0300

    задания 3-4*

--- lection 5/task_with_star.py
+++ lection 5/task_with_star.py
@@ -11,7 +11,17 @@
 
 
 def to_roman(val):
-    # Здесь нужно написать код
+    roman_str = ''
+    roman_digits = {
+        1: 'I', 2: 'II', 3: 'III', 4: 'IV', 5: 'V', 6: 'VI', 7: 'VII', 8: 'VIII', 9: 'IX',
+        10: 'X', 20: 'XX', 30: 'XXX', 40: 'XL', 50: 'L', 60: 'LX', 70: 'LXX', 80: 'LXXX', 90: 'XC',
+        100: 'C', 200: 'CC', 300: 'CCC', 400: 'CD', 500: 'D', 600: 'DC', 700: 'DCC', 800: 'DCCC', 900: 'CM',
+        1000: 'M', 2000: 'MM', 3000: 'MMM'
+    }
+    for idx, value in enumerate(reversed(str(val))):
+        digit = int(value) * 10 ** idx
+        if roman_digits.get(digit) is not None:
+            roman_str = str(roman_digits.get(digit)) + roman_str
     return roman_str
 
 # Ниже НИЧЕГО НЕ НАДО ИЗМЕНЯТЬ
